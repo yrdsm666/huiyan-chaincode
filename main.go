c18f5d5d15fb5e62a7bc96ec1c5eeb9680c49ead
yrdsm666 <47294670@qq.com>
2022-07-13 20:48:08 +0800

    init commit

---
+++ main.go
@@ -0,0 +1,303 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+
+	"github.com/hyperledger/fabric-chaincode-go/shim"
+	"github.com/hyperledger/fabric-contract-api-go/contractapi"
+)
+
+type ConfidentialMessageBySender struct {
+	Sender    string   `json:"sender"`
+	Receivers []string `json:"receivers"`
+	Message   string   `json:"message"`
+	//Number   int    `json:"number"`
+}
+
+type MessageForReceiver struct {
+	Sender   string   `json:"sender"`
+	Receiver string   `json:"receiver"`
+	Messages []string `json:"messages"`
+	//Number   int    `json:"number"`
+}
+
+type SmartContract struct {
+	contractapi.Contract
+}
+
+// sender给receiver发送消息时，为了使receiver能够知道有消息到来，需要在公共区域创建一个消息到来的通知
+func (s *SmartContract) CreateMessageNotice(ctx contractapi.TransactionContextInterface, sender string, receiver string) error {
+
+	key, err := ctx.GetStub().CreateCompositeKey("mn", []string{receiver, sender})
+	if err != nil {
+		return fmt.Errorf("failed to create composite key: %s", err.Error())
+	}
+
+	messageNoticeBytes, err := ctx.GetStub().GetState(key)
+	if err != nil {
+		return fmt.Errorf("failed to read message notice: %s", err.Error())
+	}
+	if bytes.Equal(messageNoticeBytes, []byte("1")) {
+		// notice存在，但是未被读，此时什么都不做
+		return nil
+	}
+
+	// notice不存在，说明是第一次发送消息
+	// 或者notice存在，但是已被读，此时更新
+	err = ctx.GetStub().PutState(key, []byte("1"))
+	if err != nil {
+		return fmt.Errorf("failed to put value: %s", err.Error())
+	}
+	return nil
+}
+
+// receiver读消息时，查找消息通知确定谁给自己发消息了
+func (s *SmartContract) ReadMessageNotice(ctx contractapi.TransactionContextInterface, receiver string) ([]string, error) {
+
+	rs, err := ctx.GetStub().GetStateByPartialCompositeKey("mn", []string{receiver})
+	if err != nil {
+		return nil, fmt.Errorf("failed to create composite key: %s", err.Error())
+	}
+	defer rs.Close()
+
+	var senders []string
+
+	for rs.HasNext() {
+		item, err := rs.Next()
+		if err != nil {
+			return nil, err
+		}
+		// fmt.Println("item.Key, item.Value: ")
+		// fmt.Println(item.Key, item.Value)
+
+		if bytes.Equal(item.Value, []byte("1")) {
+			// notice存在，未被读，说明来了新消息
+			_, keyPart, err := ctx.GetStub().SplitCompositeKey(item.Key)
+			if err != nil {
+				return nil, fmt.Errorf("failed to split composite key: %s", err.Error())
+			}
+			sender := keyPart[1]
+			senders = append(senders, sender)
+
+			key, err := ctx.GetStub().CreateCompositeKey("mn", []string{receiver, sender})
+			if err != nil {
+				return nil, fmt.Errorf("failed to create composite key: %s", err.Error())
+			}
+
+			err = ctx.GetStub().PutState(key, []byte("0"))
+			if err != nil {
+				return nil, fmt.Errorf("failed to put value: %s", err.Error())
+			}
+		} else {
+			// notice存在，已被读，还是原来的旧消息
+			// 这时候如果后端数据库没有保存已读的消息，那么还是需要从链上读
+			// 否则不需要，正常来讲应该不需要了。但是这里假设后端为了简便没保存已读消息
+			_, keyPart, err := ctx.GetStub().SplitCompositeKey(item.Key)
+			if err != nil {
+				return nil, fmt.Errorf("failed to split composite key: %s", err.Error())
+			}
+			sender := keyPart[0]
+			senders = append(senders, sender)
+		}
+	}
+	return senders, nil
+}
+
+func (s *SmartContract) CreateConfidentialMessageBySender(ctx contractapi.TransactionContextInterface) error {
+
+	transMap, err := ctx.GetStub().GetTransient()
+	if err != nil {
+		return fmt.Errorf("Error getting transient: " + err.Error())
+	}
+
+	// Marble properties are private, therefore they get passed in transient field
+	transientMessageJSON, ok := transMap["message"]
+	if !ok {
+		return fmt.Errorf("message not found in the transient map")
+	}
+
+	var messageInput ConfidentialMessageBySender
+	err = json.Unmarshal(transientMessageJSON, &messageInput)
+	if err != nil {
+		return fmt.Errorf("failed to unmarshal JSON: %s", err.Error())
+	}
+
+	if len(messageInput.Sender) == 0 {
+		return fmt.Errorf("sender field must be a non-empty string")
+	}
+	if len(messageInput.Receivers) == 0 {
+		return fmt.Errorf("receivers field must be a non-empty string")
+	}
+	if len(messageInput.Message) == 0 {
+		return fmt.Errorf("message field must be a non-empty string")
+	}
+
+	// Get the MSP ID of submitting client identity
+	clientMSPID, err := ctx.GetClientIdentity().GetMSPID()
+	if err != nil {
+		return fmt.Errorf("failed to get verified MSPID: %v", err)
+	}
+	if messageInput.Sender+"MSP" != clientMSPID {
+		return fmt.Errorf("sender %s and client MSPID %s is not match: %v", messageInput.Sender, clientMSPID, err)
+	}
+
+	err = verifyClientOrgMatchesPeerOrg(ctx)
+	if err != nil {
+		return fmt.Errorf("CreateMessage cannot be performed: Error %v", err)
+	}
+
+	for i := 0; i < len(messageInput.Receivers); i++ {
+		// ==== Check if message already exists ====
+		var messages []string
+
+		oldMessageAsBytes, err := ctx.GetStub().GetPrivateData(messageInput.Sender+"MSPCollection", messageInput.Receivers[i])
+		if err != nil {
+			return fmt.Errorf("Failed to get message: " + err.Error())
+		} else if oldMessageAsBytes != nil {
+			var oldMessage MessageForReceiver
+			err = json.Unmarshal(oldMessageAsBytes, &oldMessage)
+			if err != nil {
+				return fmt.Errorf("failed to unmarshal JSON: %v", err)
+			}
+			messages = oldMessage.Messages
+			messages = append(messages, messageInput.Message)
+		} else if oldMessageAsBytes == nil {
+			messages = append(messages, messageInput.Message)
+		}
+
+		// ==== Create message object, marshal to JSON, and update to state ====
+		newMessage := MessageForReceiver{
+			Sender:   messageInput.Sender,
+			Receiver: messageInput.Receivers[i],
+			Messages: messages,
+		}
+
+		newMessageJSONasBytes, err := json.Marshal(newMessage)
+		if err != nil {
+			return fmt.Errorf(err.Error())
+		}
+
+		err = ctx.GetStub().PutPrivateData(messageInput.Sender+"MSPCollection", messageInput.Receivers[i], newMessageJSONasBytes)
+		if err != nil {
+			return fmt.Errorf("failed to put Marble: %s", err.Error())
+		}
+
+		err = s.CreateMessageNotice(ctx, newMessage.Sender, newMessage.Receiver)
+		if err != nil {
+			return fmt.Errorf("failed to create message notice: %s", err.Error())
+		}
+	}
+	return nil
+}
+
+func (s *SmartContract) ReadAllConfidentialMessageByReceiver(ctx contractapi.TransactionContextInterface, receiver string) ([]MessageForReceiver, error) {
+	// Get the MSP ID of submitting client identity
+	clientMSPID, err := ctx.GetClientIdentity().GetMSPID()
+	if err != nil {
+		return nil, fmt.Errorf("failed to get verified MSPID: %v", err)
+	}
+	if receiver+"MSP" != clientMSPID {
+		return nil, fmt.Errorf("receiver is client MSPID is not match: %v", err)
+	}
+
+	// err = verifyClientOrgMatchesPeerOrg(ctx)
+	// if err != nil {
+	// 	return nil, fmt.Errorf("CreateMessage cannot be performed: Error %v", err)
+	// }
+
+	senders, err := s.ReadMessageNotice(ctx, receiver)
+	if err != nil {
+		return nil, fmt.Errorf("%s read message notice failed: %v", receiver, err)
+	}
+
+	var messageToReceivers []MessageForReceiver
+	for i := 0; i < len(senders); i++ {
+		messages, err := s.ReadConfidentialMessage(ctx, senders[i], receiver)
+		if err != nil {
+			return nil, fmt.Errorf("%s read %s message failed: %v", receiver, senders[i], err)
+		}
+		newMessage := MessageForReceiver{
+			Sender:   senders[i],
+			Receiver: receiver,
+			Messages: messages,
+		}
+
+		messageToReceivers = append(messageToReceivers, newMessage)
+	}
+	return messageToReceivers, nil
+}
+
+func (s *SmartContract) ReadConfidentialMessage(ctx contractapi.TransactionContextInterface, sender string, receiver string) ([]string, error) {
+	// Get the MSP ID of submitting client identity
+	clientMSPID, err := ctx.GetClientIdentity().GetMSPID()
+	if err != nil {
+		return nil, fmt.Errorf("failed to get verified MSPID: %v", err)
+	}
+	if receiver+"MSP" != clientMSPID {
+		return nil, fmt.Errorf("receiver and client MSPID is not match: %v", err)
+	}
+
+	// err = verifyClientOrgMatchesPeerOrg(ctx)
+	// if err != nil {
+	// 	return nil, fmt.Errorf("CreateMessage cannot be performed: Error %v", err)
+	// }
+
+	messageAsBytes, err := ctx.GetStub().GetPrivateData(sender+"MSPCollection", receiver)
+	if err != nil {
+		return nil, fmt.Errorf("Failed to get message: " + err.Error())
+	} else if messageAsBytes == nil {
+		return nil, fmt.Errorf("there is no messgae to %s in %s", receiver, sender+"collection")
+	}
+
+	var message MessageForReceiver
+	err = json.Unmarshal(messageAsBytes, &message)
+	if err != nil {
+		return nil, fmt.Errorf("failed to unmarshal JSON: %v", err)
+	}
+
+	return message.Messages, nil
+}
+
+// MessageNoticeExists returns true when messageNotice with given ID exists in world state
+func (s *SmartContract) MessageNoticeExists(ctx contractapi.TransactionContextInterface, key string) (bool, error) {
+	messageNoticeJSON, err := ctx.GetStub().GetState(key)
+	if err != nil {
+		return false, fmt.Errorf("failed to read from world state: %v", err)
+	}
+
+	return messageNoticeJSON != nil, nil
+}
+
+// verifyClientOrgMatchesPeerOrg is an internal function used verify client org id and matches peer org id.
+func verifyClientOrgMatchesPeerOrg(ctx contractapi.TransactionContextInterface) error {
+	clientMSPID, err := ctx.GetClientIdentity().GetMSPID()
+	if err != nil {
+		return fmt.Errorf("failed getting the client's MSPID: %v", err)
+	}
+	peerMSPID, err := shim.GetMSPID()
+	if err != nil {
+		return fmt.Errorf("failed getting the peer's MSPID: %v", err)
+	}
+
+	if clientMSPID != peerMSPID {
+		return fmt.Errorf("client from org %v is not authorized to read or write private data from an org %v peer", clientMSPID, peerMSPID)
+	}
+
+	return nil
+}
+
+func main() {
+
+	chaincode, err := contractapi.NewChaincode(new(SmartContract))
+
+	if err != nil {
+		fmt.Printf("Error creating private mables chaincode: %s", err.Error())
+		return
+	}
+
+	if err := chaincode.Start(); err != nil {
+		fmt.Printf("Error starting private mables chaincode: %s", err.Error())
+	}
+}
